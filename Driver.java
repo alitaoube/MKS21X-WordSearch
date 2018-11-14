9bd2285dde010e2f6f2ae71423227b254ed04eac
Ali Taoube <ataoube00@stuy.edu>
2018-11-13 22:22:30 -0500

    Edited driver so that it returns a message when you have the incorrect number of args

--- Driver.java
+++ Driver.java
@@ -1,9 +1,19 @@
+import java.util.*; //random, scanner, arraylist
+import java.io.*; //file, filenotfoundexception
+
 public class Driver{
   public static void main(String[] args) {
-    // WordSearch test = new WordSearch(25, 25, "words.txt");
-    // System.out.println(test.toString());
 
-    WordSearch test1 = new WordSearch(25, 25, "words.txt", -2061146836);
-    System.out.println(test1.toString());
+    if (args.length < 3){
+      System.out.println(
+      "Please input the dimensions and the name of the text file. i.e. java WordSearch xx yy words.txt");
+    }
+    else{
+      if (args.length == 3){
+          WordSearch test = new WordSearch(Integer.parseInt(args[0]),
+          Integer.parseInt(args[1]), args[2]);
+          System.out.println(test.toString());
+      }
+    }
   }
 }
