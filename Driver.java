1d9993374bb14e03f6c602d0b9eef9e136c04dd3
Ali Taoube <ataoube00@stuy.edu>
2018-11-14 12:19:45 -0500

    Added comprehensive try/catch statements, program should never throw an error, it should always catch it and return a statement

--- Driver.java
+++ Driver.java
@@ -10,12 +10,41 @@ public class Driver{
       "Please input the dimensions and the name of the text file. i.e. java WordSearch xx yy words.txt");
     }
     else if (args.length == 4){
-      WordSearch test = new WordSearch(Integer.parseInt(args[0]),
-      Integer.parseInt(args[1]), args[2], Integer.parseInt(args[3]), false);
-      System.out.println(test.toString());
+      try{
+        WordSearch test = new WordSearch(Integer.parseInt(args[0]),
+        Integer.parseInt(args[1]), args[2], Integer.parseInt(args[3]), false);
+        System.out.println(test.toString());
+      }
+      catch(NumberFormatException e){
+        try{
+          Integer.parseInt(args[0]);
+          Integer.parseInt(args[1]);
+        }
+        catch(NumberFormatException f){
+          System.out.println("The first two numbers are the rows and columns, so they must be integers");
+        }
+        // if (!(args[2] instanceof String)){
+            // System.out.println("The third parameter is the name of the text file you wish to use for the wordsearch, so it must be a string");
+        // }
+        try{
+          Integer.parseInt(args[3]);
+        }
+        catch(NumberFormatException g){
+          System.out.println("The fourth number is the seed of a previous puzzle if you would like to see it again, so it must be an integer");
+        }
+      }
     }
     else{
       if (args.length == 5){
+          // if (!(args[2] instanceof String)){
+              // System.out.println("The third parameter is the name of the text file you wish to use for the wordsearch, so it must be a string");
+          // }
+          try{
+            Integer.parseInt(args[3]);
+          }
+          catch(NumberFormatException g){
+            System.out.println("The fourth number is the seed of a previous puzzle if you would like to see it again, so it must be an integer");
+          }
         try{
           if (args[4].equals("key")){
             WordSearch test = new WordSearch(Integer.parseInt(args[0]),
@@ -29,7 +58,13 @@ public class Driver{
           }
         }
         catch (NumberFormatException e){
-          System.out.println("Please input integer for the row and columns (first two parameters)");
+          try{
+            Integer.parseInt(args[0]);
+            Integer.parseInt(args[1]);
+          }
+          catch(NumberFormatException f){
+            System.out.println("Please input integer for the row and columns (first two parameters)");
+          }
         }
       }
     }
@@ -37,8 +72,19 @@ public class Driver{
       // int rows, int cols, String fileName, int Randseed, boolean key
       Random randgen = new Random();
       int seed = randgen.nextInt();
-      WordSearch test = new WordSearch(Integer.parseInt(args[0]), Integer.parseInt(args[1]), args[2], seed, false);
-      System.out.println(test.toString());
+      try{
+        WordSearch test = new WordSearch(Integer.parseInt(args[0]), Integer.parseInt(args[1]), args[2], seed, false);
+        System.out.println(test.toString());
+      }
+      catch(NumberFormatException e){
+        try{
+          Integer.parseInt(args[0]);
+          Integer.parseInt(args[1]);
+        }
+        catch(NumberFormatException f){
+          System.out.println("The first two numbers are the rows and columns, so they must be integers");
+        }
     }
   }
+}
 }
